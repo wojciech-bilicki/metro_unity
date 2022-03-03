8bea75b15d96f7d947ad3ffdbd51722084906fa6
Wojciech Bilicki <wojciech.bilicki@oke.pl>
2022-03-03 16:37:30 +0100

    add PlayerControls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    // Start is called before the first frame update
+    public Rigidbody2D theRB;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Transform groundPoint;
+    private bool _isOnGround;
+    public LayerMask whatIsGround;
+
+    public Animator anim;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+        _isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
+
+        if (Input.GetButtonDown("Jump") && _isOnGround)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+        anim.SetBool("isOnGround", _isOnGround);
+
+        anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+
+
+
+
     }
 }
